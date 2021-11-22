c61464696a190f8db13cadce063c87ddf69dba79
Agent5S <19892761+Agent5S@users.noreply.github.com>
2021-11-21 22:57:53 -0600

    Display actual values on Start

--- Assets/Scripts/Game/DisplayState.cs
+++ Assets/Scripts/Game/DisplayState.cs
@@ -1,21 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DisplayState : MonoBehaviour
 {
+    static char[] Symbols = { 'A', 'B', 'C', 'D' };
+
     public GameObject buttonPrefab;
 
     private GameState state;
+    private TextMeshProUGUI[] displays;
 
     private void Awake()
     {
         this.state = GetComponent<GameState>();
+        var n = state.columns * state.columns;
+        this.displays = new TextMeshProUGUI[n];
 
-        for (int i = 0; i < state.columns * state.columns; i++)
+        for (int i = 0; i < n; i++)
         {
             GameObject button = Instantiate(buttonPrefab);
+            TextMeshProUGUI display = button
+                .GetComponentInChildren<TextMeshProUGUI>();
+            this.displays[i] = display;
             button.transform.SetParent(transform);
+        }
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < displays.Length - 1; i++)
+        {
+            var display = displays[i];
+            display.text = $"{Symbols[state.board[i]]}";
         }
     }
 }
