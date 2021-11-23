5d26704a1052f64cc4fc7c2e5a1b71a733093ad6
Agent5S <19892761+Agent5S@users.noreply.github.com>
2021-11-23 07:44:02 -0600

    Refactor to use as a singleton and not a MonoBehaviour

--- Assets/Scripts/Game/GameState.cs
+++ Assets/Scripts/Game/GameState.cs
@@ -3,28 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-public class GameState : MonoBehaviour
+public class GameState
 {
-    //TODO: allow for more values to be added dynamically
-    private static int[] PossibleValues = { 0, 1, 2, 3 };
-    public int columns = 8;
+    public delegate void UpdateBoard();
+    public static event UpdateBoard OnBoardUpdated;
 
-    //Use a value less than -8 as empty value to prevent bugs
-    public int selectedIndex = -10;
-    private int[] _board;
-    public int[] board
+    public delegate void UpdateSelected();
+    public static event UpdateSelected OnSelectUpdated;
+
+    public static readonly int[] PossibleValues = { 0, 1, 2, 3 };
+    public static readonly int Columns = 8;
+    public static GameState Global;
+
+    private int[] board;
+    public int[] Board
     {
-        private set => _board = value;
-        get => _board;
+        get => board;
     }
 
-    private DisplayState display;
-
-    private void Awake()
+    //Use a value less than -8 as empty value to prevent bugs
+    private int selectedIdx = -10;
+    public int SelectedIdx
     {
-        this.display = GetComponent<DisplayState>();
+        get => selectedIdx;
+        set
+        {
+            this.selectedIdx = value;
+            OnSelectUpdated?.Invoke();
+        }
+    }
 
-        board = new int[columns * columns];
+    //Utility array to simulate copy semantics 
+    private int[] boardCopy = new int[Columns * Columns];
+    private void SwapBoards()
+    {
+        var copy = boardCopy;
+        boardCopy = board;
+        board = copy;
+        OnBoardUpdated?.Invoke();
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void CreateState()
+    {
+        Global = new GameState();
+        var newBoard = new int[Columns * Columns];
 
         int[] availableValues = new int[PossibleValues.Length];
         int lastValidIndex;
@@ -34,7 +57,7 @@ public class GameState : MonoBehaviour
         var left = new int[] { -1, -2 };
 
         var xy = new int[] { 0, 0 };
-        for (int i = 0; i < board.Length; i++)
+        for (int i = 0; i < newBoard.Length; i++)
         {
             Array.Copy(PossibleValues, availableValues, PossibleValues.Length);
             lastValidIndex = PossibleValues.Length - 1;
@@ -64,36 +87,39 @@ public class GameState : MonoBehaviour
                 UnityEngine.Random.value * lastValidIndex
             );
             var selectedValue = availableValues[selectedIndex];
-            board[i] = selectedValue;
+            newBoard[i] = selectedValue;
 
             //Calculate next iteration's adjacent values
             var nextIndex = i + 1;
-            var y_2 = nextIndex - columns - columns;
-            xy[1] = Math.DivRem(nextIndex, columns, out xy[0]);
+            var y_2 = nextIndex - Columns - Columns;
+            xy[1] = Math.DivRem(nextIndex, Columns, out xy[0]);
             //FIXME: This generates garbage
             left = xy[0] == 0 ?
-                new int[]{ -1, -2 } :
-                new int[]{ selectedValue, left[0] };
+                new int[] { -1, -2 } :
+                new int[] { selectedValue, left[0] };
             top = xy[1] == 0 ?
                 new int[] { -1, -2 } :
-                new int[] { board[nextIndex - columns],
-                    y_2 < 0 ? -1 : board[y_2] };
+                new int[] { newBoard[nextIndex - Columns],
+                    y_2 < 0 ? -1 : newBoard[y_2] };
         }
+        Global.board = newBoard;
     }
 
     public void Select(int newIndex)
     {
-        var indexDelta = Mathf.Abs(newIndex - selectedIndex);
-        if (indexDelta == 1 || indexDelta == 8)
+        Array.Copy(board, boardCopy, board.Length);
+
+        var indexDiff = Mathf.Abs(newIndex - SelectedIdx);
+        if (indexDiff == 1 || indexDiff == 8)
         {
-            var store = board[newIndex];
-            this.board[newIndex] = board[selectedIndex];
-            this.board[selectedIndex] = store;
-            selectedIndex = -10;
-            display.UpdateButtons();
+            var temp = boardCopy[newIndex];
+            this.boardCopy[newIndex] = boardCopy[SelectedIdx];
+            this.boardCopy[SelectedIdx] = temp;
+            SelectedIdx = -10;
+            SwapBoards();
             return;
         }
 
-        selectedIndex = newIndex;
+        SelectedIdx = newIndex;
     }
 }
