264f6f73275f48b2844b302bf09c4b3b54f5f2f8
Agent5S <19892761+Agent5S@users.noreply.github.com>
2021-11-24 00:35:05 -0600

    Display game over message

---
+++ Assets/Scripts/Game Over/DisplayMessage.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DisplayMessage : MonoBehaviour
+{
+    public TextMeshProUGUI message;
+    public TextMeshProUGUI score;
+
+    private void Start()
+    {
+        var won = GameState.Global.Score > GameState.Goal;
+        var message = won ? "Buen trabajo!" : "Mejor suerte a próxima";
+        this.message.text = message;
+        this.score.text = $"{GameState.Global.Score} / {GameState.Goal}";
+    }
+}
